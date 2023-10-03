beac67399ba3bb85d724155b30f33ef0241123e5
Utku Toraman <utkutoraman@gmail.com>
2023-10-04 00:09:11 +0300

    update percentages

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -9,11 +9,11 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     roll = random.random() * 100
-    if roll < 15:
+    if roll < 20:
         raise HTTPException(status_code=500, detail="[500] Internal Server Error")
-    elif 15 <= roll < 30:
+    elif 20 <= roll < 40:
         raise HTTPException(status_code=400, detail="[400] Bad Request")
-    elif 30 <= roll < 45:
+    elif 40 <= roll < 60:
         raise HTTPException(status_code=404, detail="[404] Not Found")
     
     return {"message": "[200] OK"}
